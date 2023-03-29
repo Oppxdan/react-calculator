47f8f3e01e70b1925a59cf29ea135298b25756d3
Oppxdan <regalado2829@gmail.com>
2023-03-29 11:37:34 -0400

    Adding Styling to Calculator

--- src/components/calculator/calculator.component.jsx
+++ src/components/calculator/calculator.component.jsx
@@ -1,30 +1,32 @@
+import './calculator.styles.css';
+
 function Calculator(props) {
 
 
     return (
         <div className="calculator">
-                <div>
-                    {props.result}
+                <div className="output">
+                    <h2>{props.result}</h2>
                 </div>
             <section className="calc-grid">
                 <button className="calc-btn" onClick={() => props.addNumber("7")}>7</button>
                 <button className="calc-btn" onClick={() => props.addNumber("8")}>8</button>
                 <button className="calc-btn" onClick={() => props.addNumber("9")}>9</button>
-                <button className="calc-btn" onClick={() => props.addNumber("/")}>/</button> 
+                <button className="calc-btn operand" onClick={() => props.addNumber("/")}>/</button> 
                 <button className="calc-btn" onClick={() => props.addNumber("4")}>4</button> 
                 <button className="calc-btn" onClick={() => props.addNumber("5")}>5</button> 
                 <button className="calc-btn" onClick={() => props.addNumber("6")}>6</button> 
-                <button className="calc-btn" onClick={() => props.addNumber("*")}>X</button> 
+                <button className="calc-btn operand" onClick={() => props.addNumber("*")}>X</button> 
                 <button className="calc-btn" onClick={() => props.addNumber("1")}>1</button> 
                 <button className="calc-btn" onClick={() => props.addNumber("2")}>2</button> 
                 <button className="calc-btn" onClick={() => props.addNumber("3")}>3</button> 
-                <button className="calc-btn" onClick={() => props.addNumber("-")}>-</button> 
+                <button className="calc-btn operand" onClick={() => props.addNumber("-")}>-</button> 
                 <button className="calc-btn" onClick={() => props.addNumber("0")}>0</button> 
                 <button className="calc-btn" onClick={() => props.addDot(".")}>.</button> 
                 <button className="calc-btn" onClick={props.calculate}>=</button> 
-                <button className="calc-btn" onClick={() => props.addNumber("+")}>+</button> 
-                <button className="calc-btn" onClick={props.backspace}>⌫</button>
-                <button className="calc-btn" onClick={props.clear}>C</button>  
+                <button className="calc-btn operand" onClick={() => props.addNumber("+")}>+</button> 
+                <button className="calc-btn red-btn" onClick={props.backspace}>⌫</button>
+                <button className="calc-btn red-btn" onClick={props.clear}>AC</button>  
             </section>
         </div>
     )
