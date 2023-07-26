d98771de2f6e142939fc8b59ce32925a7ef81739
Oppxdan <regalado2829@gmail.com>
2023-07-26 16:38:38 -0400

    Pulling

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Calculator from './components/calculator/calculator.component';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Example test case for addition
+test('calculates addition correctly', () => {
+  // Render the calculator component
+  render(<Calculator />);
+
+  // Find the number keys and the equal button
+  const twoButton = screen.getByText('2');
+  const plusButton = screen.getByText('+');
+  const threeButton = screen.getByText('3');
+  const equalButton = screen.getByText('=');
+
+  // Simulate user interaction: 2 + 3 =
+  fireEvent.click(twoButton);
+  fireEvent.click(plusButton);
+  fireEvent.click(threeButton);
+  fireEvent.click(equalButton);
+
+  // Assert the result displayed on the calculator screen
+  const resultDisplay = screen.getByTestId('result-display');
+  expect(resultDisplay).toHaveTextContent('5');
 });
--- src/components/calculator/calculator.component.jsx
+++ src/components/calculator/calculator.component.jsx
@@ -1,35 +1,79 @@
 import './calculator.styles.css';
+import { useState } from 'react';
 
 function Calculator(props) {
+  const [hasDecimal, setHasDecimal] = useState(false);
+  const { result, setResult } = props;
 
+  const handleNumberClick = (number) => {
+    if (number === '.') {
+      if (hasDecimal) {
+        return;
+      }
+      setHasDecimal(true);
+    } else {
+      setHasDecimal(false);
+    }
 
-    return (
-        <div className="calculator">
-                <div className="output">
-                    <h2>{props.result}</h2>
-                </div>
-            <section className="calc-grid">
-                <button className="calc-btn" onClick={() => props.addNumber("7")}>7</button>
-                <button className="calc-btn" onClick={() => props.addNumber("8")}>8</button>
-                <button className="calc-btn" onClick={() => props.addNumber("9")}>9</button>
-                <button className="calc-btn operand" onClick={() => props.addNumber("/")}>/</button> 
-                <button className="calc-btn" onClick={() => props.addNumber("4")}>4</button> 
-                <button className="calc-btn" onClick={() => props.addNumber("5")}>5</button> 
-                <button className="calc-btn" onClick={() => props.addNumber("6")}>6</button> 
-                <button className="calc-btn operand" onClick={() => props.addNumber("*")}>X</button> 
-                <button className="calc-btn" onClick={() => props.addNumber("1")}>1</button> 
-                <button className="calc-btn" onClick={() => props.addNumber("2")}>2</button> 
-                <button className="calc-btn" onClick={() => props.addNumber("3")}>3</button> 
-                <button className="calc-btn operand" onClick={() => props.addNumber("-")}>-</button> 
-                <button className="calc-btn" onClick={() => props.addNumber("0")}>0</button> 
-                <button className="calc-btn" onClick={() => props.addDot(".")}>.</button> 
-                <button className="calc-btn" onClick={props.calculate}>=</button> 
-                <button className="calc-btn operand" onClick={() => props.addNumber("+")}>+</button> 
-                <button className="calc-btn red-btn" onClick={props.backspace}>⌫</button>
-                <button className="calc-btn red-btn" onClick={props.clear}>AC</button>  
-            </section>
+    const newResult = result + number;
+    setResult(newResult);
+  };
+
+  const handleOperatorClick = (operator) => {
+    const newResult = result.endsWith('.') ? result.slice(0, -1) + operator : result + operator;
+    setResult(newResult);
+  };
+
+  const handleCalculateClick = () => {
+    try {
+      const calculation = String(eval(result));
+      setResult(calculation);
+    } catch (error) {
+      setResult('Error');
+    }
+  };
+
+  const handleClearClick = () => {
+    setResult('');
+  };
+
+  const handleBackspaceClick = () => {
+    const backspacedResult = result.substring(0, result.length - 1);
+    setResult(backspacedResult);
+  };
+
+  return (
+    <section className="calculator">
+      <div className="output">
+        <h2 aria-live="polite" aria-atomic="true" role="alert">
+          {result}
+        </h2>
+      </div>
+      <form>
+        <div className="calc-grid">
+          <button className="calc-btn" onClick={() => handleNumberClick('7')} type="button" aria-label="7">7</button>
+          <button className="calc-btn" onClick={() => handleNumberClick('8')} type="button" aria-label="8">8</button>
+          <button className="calc-btn" onClick={() => handleNumberClick('9')} type="button" aria-label="9">9</button>
+          <button className="calc-btn operand" onClick={() => handleOperatorClick('/')} type="button" aria-label="Divide">÷</button>
+          <button className="calc-btn" onClick={() => handleNumberClick('4')} type="button" aria-label="4">4</button>
+          <button className="calc-btn" onClick={() => handleNumberClick('5')} type="button" aria-label="5">5</button>
+          <button className="calc-btn" onClick={() => handleNumberClick('6')} type="button" aria-label="6">6</button>
+          <button className="calc-btn operand" onClick={() => handleOperatorClick('*')} type="button" aria-label="Multiply">×</button>
+          <button className="calc-btn" onClick={() => handleNumberClick('1')} type="button" aria-label="1">1</button>
+          <button className="calc-btn" onClick={() => handleNumberClick('2')} type="button" aria-label="2">2</button>
+          <button className="calc-btn" onClick={() => handleNumberClick('3')} type="button" aria-label="3">3</button>
+          <button className="calc-btn operand" onClick={() => handleOperatorClick('-')} type="button" aria-label="Subtract">-</button>
+          <button className="calc-btn" onClick={() => handleNumberClick('0')} type="button" aria-label="0">0</button>
+          <button className="calc-btn" onClick={() => handleNumberClick('.')} type="button" aria-label="Decimal Point">.</button>
+          <button className="calc-btn" onClick={handleCalculateClick} type="button" aria-label="Equals">=</button>
+          <button className="calc-btn operand" onClick={() => handleOperatorClick('+')} type="button" aria-label="Add">+</button>
+          <button className="calc-btn red-btn" onClick={handleBackspaceClick} type="button" aria-label="Backspace">⌫</button>
+          <button className="calc-btn red-btn" onClick={handleClearClick} type="button" aria-label="Clear">AC</button>
+
         </div>
-    )
+      </form>
+    </section>
+  );
 }
 
 export default Calculator;
