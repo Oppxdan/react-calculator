f57125acd0c6bf78311b60b7b954b05f2696dca6
Oppxdan <regalado2829@gmail.com>
2023-07-26 16:47:00 -0400

    Formatting After Accessibility Changes
    After Adding Semantic "HTML" [JSX, In actuality], aria labels, and
    merging as needed, I am now simply formatting the code in the calculator
    component.

--- src/components/calculator/calculator.component.jsx
+++ src/components/calculator/calculator.component.jsx
@@ -51,25 +51,150 @@ function Calculator(props) {
       </div>
       <form>
         <div className="calc-grid">
-          <button className="calc-btn" onClick={() => handleNumberClick('7')} type="button" aria-label="7">7</button>
-          <button className="calc-btn" onClick={() => handleNumberClick('8')} type="button" aria-label="8">8</button>
-          <button className="calc-btn" onClick={() => handleNumberClick('9')} type="button" aria-label="9">9</button>
-          <button className="calc-btn operand" onClick={() => handleOperatorClick('/')} type="button" aria-label="Divide">÷</button>
-          <button className="calc-btn" onClick={() => handleNumberClick('4')} type="button" aria-label="4">4</button>
-          <button className="calc-btn" onClick={() => handleNumberClick('5')} type="button" aria-label="5">5</button>
-          <button className="calc-btn" onClick={() => handleNumberClick('6')} type="button" aria-label="6">6</button>
-          <button className="calc-btn operand" onClick={() => handleOperatorClick('*')} type="button" aria-label="Multiply">×</button>
-          <button className="calc-btn" onClick={() => handleNumberClick('1')} type="button" aria-label="1">1</button>
-          <button className="calc-btn" onClick={() => handleNumberClick('2')} type="button" aria-label="2">2</button>
-          <button className="calc-btn" onClick={() => handleNumberClick('3')} type="button" aria-label="3">3</button>
-          <button className="calc-btn operand" onClick={() => handleOperatorClick('-')} type="button" aria-label="Subtract">-</button>
-          <button className="calc-btn" onClick={() => handleNumberClick('0')} type="button" aria-label="0">0</button>
-          <button className="calc-btn" onClick={() => handleNumberClick('.')} type="button" aria-label="Decimal Point">.</button>
-          <button className="calc-btn" onClick={handleCalculateClick} type="button" aria-label="Equals">=</button>
-          <button className="calc-btn operand" onClick={() => handleOperatorClick('+')} type="button" aria-label="Add">+</button>
-          <button className="calc-btn red-btn" onClick={handleBackspaceClick} type="button" aria-label="Backspace">⌫</button>
-          <button className="calc-btn red-btn" onClick={handleClearClick} type="button" aria-label="Clear">AC</button>
-
+          <button
+            className="calc-btn"
+            onClick={() => handleNumberClick("7")}
+            type="button"
+            aria-label="7"
+          >
+            7
+          </button>
+          <button
+            className="calc-btn"
+            onClick={() => handleNumberClick("8")}
+            type="button"
+            aria-label="8"
+          >
+            8
+          </button>
+          <button
+            className="calc-btn"
+            onClick={() => handleNumberClick("9")}
+            type="button"
+            aria-label="9"
+          >
+            9
+          </button>
+          <button
+            className="calc-btn operand"
+            onClick={() => handleOperatorClick("/")}
+            type="button"
+            aria-label="Divide"
+          >
+            ÷
+          </button>
+          <button
+            className="calc-btn"
+            onClick={() => handleNumberClick("4")}
+            type="button"
+            aria-label="4"
+          >
+            4
+          </button>
+          <button
+            className="calc-btn"
+            onClick={() => handleNumberClick("5")}
+            type="button"
+            aria-label="5"
+          >
+            5
+          </button>
+          <button
+            className="calc-btn"
+            onClick={() => handleNumberClick("6")}
+            type="button"
+            aria-label="6"
+          >
+            6
+          </button>
+          <button
+            className="calc-btn operand"
+            onClick={() => handleOperatorClick("*")}
+            type="button"
+            aria-label="Multiply"
+          >
+            ×
+          </button>
+          <button
+            className="calc-btn"
+            onClick={() => handleNumberClick("1")}
+            type="button"
+            aria-label="1"
+          >
+            1
+          </button>
+          <button
+            className="calc-btn"
+            onClick={() => handleNumberClick("2")}
+            type="button"
+            aria-label="2"
+          >
+            2
+          </button>
+          <button
+            className="calc-btn"
+            onClick={() => handleNumberClick("3")}
+            type="button"
+            aria-label="3"
+          >
+            3
+          </button>
+          <button
+            className="calc-btn operand"
+            onClick={() => handleOperatorClick("-")}
+            type="button"
+            aria-label="Subtract"
+          >
+            -
+          </button>
+          <button
+            className="calc-btn"
+            onClick={() => handleNumberClick("0")}
+            type="button"
+            aria-label="0"
+          >
+            0
+          </button>
+          <button
+            className="calc-btn"
+            onClick={() => handleNumberClick(".")}
+            type="button"
+            aria-label="Decimal Point"
+          >
+            .
+          </button>
+          <button
+            className="calc-btn"
+            onClick={handleCalculateClick}
+            type="button"
+            aria-label="Equals"
+          >
+            =
+          </button>
+          <button
+            className="calc-btn operand"
+            onClick={() => handleOperatorClick("+")}
+            type="button"
+            aria-label="Add"
+          >
+            +
+          </button>
+          <button
+            className="calc-btn red-btn"
+            onClick={handleBackspaceClick}
+            type="button"
+            aria-label="Backspace"
+          >
+            ⌫
+          </button>
+          <button
+            className="calc-btn red-btn"
+            onClick={handleClearClick}
+            type="button"
+            aria-label="Clear"
+          >
+            AC
+          </button>
         </div>
       </form>
     </section>
